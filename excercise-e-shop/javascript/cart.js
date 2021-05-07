40070f8c4366798893ea1a95dce5db5bd395230f
st-quangnguyen2 <quang.nguyen2@supremetech.vn>
2021-05-07 14:15:51 +0700

    convert js from ES5 to ES6

--- excercise-e-shop/javascript/cart.js
+++ excercise-e-shop/javascript/cart.js
@@ -1,104 +1,8 @@
-var header = document.querySelector('header');
+import {setCart, getCart, setCartQuantity, findIndex} from './common.js';
+
+let header = document.querySelector('header');
 header.style.background = "#444444";
 header.style.position = "static";
-
-/**
- * Compute quantity of products in cart. Then display it at header.
- */
-function setCartQuantity() {
-  document.querySelector('.cart-quantity').innerHTML = getCart().reduce(function (totalQuantity, cartItem) {
-    return totalQuantity + cartItem.quantity;
-  }, 0);
-}
-
-/**
- * Update cart in localStorage.
- */
-function setCart(cart) {
-  localStorage.setItem('cart', JSON.stringify(cart));
-}
-
-/**
- * Return cart in localStorage.
- */
-function getCart() {
-  return localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
-}
-
-/**
- * Returns a string as a result after transform cartItem object.
- * 
- * @param {object} cartItem The item in cart.
- * @return {string} HTML Node as a string.
- */
-function convertCartItemToHTML(cartItem) {
-  return '' +
-    '<li class="list-item cart-product-item">' +
-      '<div class="cart-product fl-row fl-1">' +
-        '<div class="cart-product-img-container">' +
-          '<a href="#" class="cart-product-link">' +
-            '<img src="' + cartItem.image + '" alt="this is product image" class="cart-product-img">' +
-          '</a>' +
-        '</div>' +
-        '<div class="cart-product-body flex fl-space-between fl-1">' +
-          '<div class="cart-product-body-left">' +
-            '<a href="#" class="cart-product-link">' +
-              '<h4 class="cart-product-name">' + cartItem.name + '</h4>' +
-              '</a>' +
-              '<p class="cart-product-price">' + cartItem.price.toFixed(2) + '</p>' +
-            (cartItem.discount ? '<p class="cart-product-price-discount">' + (cartItem.price * (100 + cartItem.discount) / 100).toFixed(2) + '</p>' : '') + 
-            '</div>' + 
-          '<div class="cart-product-body-right">' + 
-          '<p class="cart-product-total-price">' + (cartItem.price * cartItem.quantity).toFixed(2) + '</p>' + 
-            '<div class="cart-product-amount-group flex">' +
-            '<button class="amount-decrease" onclick="updateCartItemQuantity(' + cartItem.id + ', ' + (cartItem.quantity - 1) + ')">-</button>' +
-              '<input class="amount-inp" value="' + cartItem.quantity + '" type="number" onchange="quantityInputChange(this, ' + cartItem.id + ', ' + cartItem.quantity + ')">' + 
-              '<button class="amount-increase" onclick="updateCartItemQuantity(' + cartItem.id + ', ' + (cartItem.quantity + 1) + ')">+</button>' +
-              '</div>' + 
-            '<div class="cart-product-remove-group">' + 
-            '<button class="btn btn-flat-primary" onclick="removeCartItem(' + cartItem.id + ')">Remove</button>' + 
-            '</div>' +
-          '</div>' +
-          '</div>' +
-      '</div>' +
-    '</li>';
-  }
-
-/**
- * Transform data in cart. Then fill in it into cart group to display.
- */
-function renderCartItem() {
-  var clearAll =  '' + 
-    '<li class="cart-product-group-action">' +
-      '<button class="btn btn-flat-primary" onclick="removeAll()">Remove all</button>' +
-    '</li>';
-  document.querySelector('.cart-product-group').innerHTML = cart.length ? cart.map(convertCartItemToHTML).join('') + clearAll : 'Hiện không có sản phẩm nào trong giỏ hàng';
-}
-
-/**
- * When quantity input onchange update quantity of item in cart has id equal passed id .
- *
- * @param {HTMLElement} target The array.
- * @param {number || string} id The id .
- * @param {number} quantity The quantity of item before input change .
- */
-function quantityInputChange(target, id, quantity) {
-  var newQuantity = +target.value || quantity;
-  updateCartItemQuantity(id, newQuantity);
-}
-
-/**
- * Returns index of element has id equal passed id in an array.
- *
- * @param {array} arr The array.
- * @param {number || string} id The id .
- * @return {number} index of element.
- */
-function findIndex(arr, id) {
-  return arr.map(function (element) {
-    return element.id;
-  }).indexOf(id);
-}
 
 /**
  * Update quantity of a cartItem in cart. Then update cart and rerender components related.
@@ -107,7 +11,7 @@ function findIndex(arr, id) {
  * @param {number } quantity The quantity of cartItem will be updated.
  */
 function updateCartItemQuantity(id, quantity) {
-  var findId = findIndex(cart, id);
+  let findId = findIndex(cart, id);
   if (quantity < 1) {
     quantity = 1;
   }
@@ -119,22 +23,144 @@ function updateCartItemQuantity(id, quantity) {
 }
 
 /**
+ * When quantity input onchange update quantity of item in cart has id equal passed id .
+ *
+ * @param {HTMLElement} target The array.
+ * @param {number || string} id The id .
+ * @param {number} quantity The quantity of item before input change .
+ */
+function quantityInputChange(target, id, quantity) {
+  let newQuantity = +target.value || quantity;
+  updateCartItemQuantity(id, newQuantity);
+}
+
+/**
  * Remove a cartItem in cart. Then update cart and rerender components related.
  *
  * @param {number || string} id The id of a cartItem.
  */
-function removeCartItem(id) {
-  cart = cart.filter(function (cartItem) {
+ function removeCartItem(id) {
+  cart = cart.filter(cartItem => {
     return cartItem.id !== id;
   });
   render();
 }
 
 /**
+ * Remove all items in cart. Then update cart in localStorage and rerender components related.
+ */
+ function removeAll() {
+  cart = [];
+  render();
+}
+
+/**
+ * Returns a string as a result after transform cartItem object.
+ * 
+ * @param {object} cartItem The item in cart.
+ * @return {string} HTML Node as a string.
+ */
+function convertCartItemToHTML(cartItem) {
+  return `
+    <li class="list-item cart-product-item">
+      <div class="cart-product fl-row fl-1">
+        <div class="cart-product-img-container">
+          <a href="#" class="cart-product-link">
+            <img src="${cartItem.image}" alt="this is product image" class="cart-product-img">
+          </a>
+        </div>
+        <div class="cart-product-body flex fl-space-between fl-1">
+          <div class="cart-product-body-left">
+            <a href="#" class="cart-product-link">
+              <h4 class="cart-product-name">${cartItem.name}</h4>
+              </a>
+              <p class="cart-product-price">${cartItem.price.toFixed(2)}</p>
+            ${cartItem.discount ? `<p class="cart-product-price-discount">${(cartItem.price * (100 + cartItem.discount) / 100).toFixed(2)}</p>` : ''}
+            </div>
+          <div class="cart-product-body-right">
+          <p class="cart-product-total-price">${(cartItem.price * cartItem.quantity).toFixed(2)}</p>
+            <div class="cart-product-amount-group flex">
+              <button class="amount-btn amount-decrease" data-id="${cartItem.id}" data-quantity="${cartItem.quantity - 1}">-</button>
+              <input class="amount-inp" value="${cartItem.quantity}" type="number" data-id="${cartItem.id}" data-quantity="${cartItem.quantity}"> 
+              <button class="amount-btn amount-increase" data-id="${cartItem.id}" data-quantity="${cartItem.quantity + 1}">+</button>
+            </div>
+            <div class="cart-product-remove-group">
+              <button class="btn btn-flat-primary btn-remove-item" data-id="${cartItem.id}">Remove</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+  `;
+}
+
+/**
+ * Add event decrease or increase quantity of cart item for buttons have class amount-btn
+ */
+function addEventForBtn() {
+  let butts = document.getElementsByClassName('amount-btn');
+  for (let butt of butts){
+    let id = +butt.getAttribute('data-id');
+    let quantity = +butt.getAttribute('data-quantity');
+    butt.addEventListener('click', () => updateCartItemQuantity(id, quantity));
+  }
+}
+
+/**
+ * Add event change quantity of cart item for inputs have class amount-inp
+ */
+function addEventForInp() {
+  let inps = document.getElementsByClassName('amount-inp');
+  for (let inp of inps) {
+    let id = +inp.getAttribute('data-id');
+    let quantity = +inp.getAttribute('data-quantity');
+    let newQuantity = +inp.value || quantity;
+    inp.addEventListener('change', () =>  updateCartItemQuantity(id, newQuantity));
+  }
+}
+
+/**
+ * Add event remove item in cart for buttons have class name btn-remove-item 
+ */
+function addEventRemoveCartItem() {
+  let butts = document.getElementsByClassName('btn-remove-item');
+  for (let butt of butts) {
+    let id = +butt.getAttribute('data-id');
+    butt.addEventListener('click', () =>  removeCartItem(id));
+  }
+}
+
+/**
+ * Add event remove all item in cart for button has class name btn-remove-all 
+ */
+function addEventRemoveAll() {
+  let butt = document.querySelector('.btn-remove-all');
+  if(butt) {
+    butt.addEventListener('click', removeAll);
+  }
+}
+
+/**
+ * Transform data in cart. Then fill in it into cart group to display and add event for it.
+ */
+function renderCartItem() {
+  let clearAll = `
+    <li class="cart-product-group-action">
+      <button class="btn btn-flat-primary btn-remove-all"">Remove all</button>
+    </li>
+    `;
+  document.querySelector('.cart-product-group').innerHTML = cart.length ? cart.map(convertCartItemToHTML).join('') + clearAll : 'Hiện không có sản phẩm nào trong giỏ hàng';
+  addEventForBtn();
+  addEventForInp();
+  addEventRemoveCartItem();
+  addEventRemoveAll();
+}
+
+/**
  * Compute total price of items in cart. Then fill in it into element has class is .total-price to display.
  */
 function renderTotalPrice() {
-  document.querySelector('.total-price').innerHTML = cart.reduce(function (total, cartItem) {
+  document.querySelector('.total-price').innerHTML = cart.reduce((total, cartItem) => {
     return total + (cartItem.price * cartItem.quantity);
   }, 0).toFixed(2);
 }
@@ -149,14 +175,5 @@ function render() {
   setCartQuantity();
 }
 
-/**
- * Remove all items in cart. Then update cart in localStorage and rerender components related.
- */
-function removeAll() {
-  cart = [];
-  render();
-}
-
-
-var cart = getCart();
+let cart = getCart();
 render();
